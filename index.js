16aba5c5b8e62334c596bbfcf9cf0cf9868fd2d8
Josue Mixalis <cacciator31@gmail.com>
2024-02-12 17:51:39 -0600

    uibuild

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/api/persons/:id',(request,response) => {
 app.delete('/api/persons/:id',(request,response) => {
   const id =  Number(request.params.id)
 
-  const person = persons.filter(person => person.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
