f998b711fa2e0caaa71b923aa2cb21f95d377c0a
Josue Mixalis <cacciator31@gmail.com>
2024-02-12 17:45:39 -0600

    uibuild

--- index.js
+++ index.js
@@ -20,10 +20,23 @@ const myMorgan = (tokens,request, response) => {
   
 }
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(cors())
-app.use(express.static('dist'))
 app.use(express.json())
+app.use(requestLogger)
 app.use(morgan(myMorgan))
+app.use(express.static('dist'))
 
 
 let persons = [
@@ -120,6 +133,8 @@ app.post('/api/persons',(request,response) => {
   response.json(person)
 
 })
+
+app.use(unknownEndpoint);
 
 const PORT = 3001
 app.listen(PORT,() => {
